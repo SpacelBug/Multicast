957e197e5be32d8f93cddbe89e9338897e19683f
Dewlish <commander.n1@mail.ru>
2021-02-20 15:57:11 +1200

    Update multicast.py

--- multicast.py
+++ multicast.py
@@ -9,7 +9,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', MCAST_PORT))  # use MCAST_GRP instead of '' to listen only
                              # to MCAST_GRP, not all groups on MCAST_PORT
-mreq = pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
