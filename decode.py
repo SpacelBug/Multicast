6ebc74d0f97fbce47e12f0fdaec627aaf3759ec8
Dewlish <commander.n1@mail.ru>
2021-02-20 14:23:17 +1200

    Update decode.py

--- decode.py
+++ decode.py
@@ -2,7 +2,7 @@
 flag=0
 fixedHeader=['']
 # Порядковый номер распакован не верно
-for i in unpack('>6B2c5s2s3s2s2H4B2H2h4Bi2H', x[0:48]):
+for i in unpack('>6B2c5s2s3s2s2H4B2H2h4Bi2H', data[0:48]):
     if (flag < 6):
         fixedHeader[0]=fixedHeader[0]+str(i)
         flag=flag+1
@@ -10,9 +10,9 @@ for i in unpack('>6B2c5s2s3s2s2H4B2H2h4Bi2H', x[0:48]):
         fixedHeader.append(i)
 
 blockette_1000=[]
-for i in unpack('>2H4B',x[48:56]):
+for i in unpack('>2H4B',data[48:56]):
     blockette_1000.append(i)
 
 blockette_1001=[]
-for i in unpack('>2HBb2B',x[56:64]):
+for i in unpack('>2HBb2B',data[56:64]):
     blockette_1001.append(i)
