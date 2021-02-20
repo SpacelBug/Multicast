cecf0c54c99eea09a4eac3928ea996473c07b521
Dewlish <commander.n1@mail.ru>
2021-02-20 13:58:27 +1200

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,18 @@
+# Распаковка первых 48 байт и blockette`ов
+flag=0
+fixedHeader=['']
+# Порядковый номер распакован не верно
+for i in unpack('>6B2c5s2s3s2s2H4B2H2h4Bi2H', x[0:48]):
+    if (flag < 6):
+        fixedHeader[0]=fixedHeader[0]+str(i)
+        flag=flag+1
+    else:
+        fixedHeader.append(i)
+
+blockette_1000=[]
+for i in unpack('>2H4B',x[48:56]):
+    blockette_1000.append(i)
+
+blockette_1001=[]
+for i in unpack('>2HBb2B',x[56:64]):
+    blockette_1001.append(i)
