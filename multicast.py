73e177bf0832231fdfe7bf8833df5c776ec79ccd
Dewlish <commander.n1@mail.ru>
2021-02-20 13:57:12 +1200

    Update multicast.py

--- multicast.py
+++ multicast.py
@@ -13,26 +13,16 @@ mreq = pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-#while True:
+# Получили с сокета данные
+data=sock.recv(512)
+print(data)
 
-x=sock.recv(512)
-print(x)
+# Записали данные в файл
+f = open('test.mseed','wb+')
+f.write(data)
+f.close()
 
-# Распаковка первых 48 байт и blockette`ов
-flag=0
-fixedHeader=['']
-# Порядковый номер распакован не верно
-for i in unpack('>6B2c5s2s3s2s2H4B2H2h4Bi2H', x[0:48]):
-    if (flag < 6):
-        fixedHeader[0]=fixedHeader[0]+str(i)
-        flag=flag+1
-    else:
-        fixedHeader.append(i)
+# Прочитали с файла
+read('test.msd')
+# Но получили ошибку формата
 
-blockette_1000=[]
-for i in unpack('>2H4B',x[48:56]):
-    blockette_1000.append(i)
-
-blockette_1001=[]
-for i in unpack('>2HBb2B',x[56:64]):
-    blockette_1001.append(i)
